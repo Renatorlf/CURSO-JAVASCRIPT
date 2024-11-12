a33678708229c1ec55cc2d4d9b2b3f7bd4cc0ebc
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2024-11-11 22:10:43 -0300

    Tipos de Saidas

---
+++ JavaScript/tipos_saidas.js
@@ -0,0 +1,9 @@
+/*Primeira forda de saída do JS*/
+/*colocando o getElementById você pega o elemento que está no arquivo HTML pelo ID, e dai ele é impresso na tela */
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+
+/*A terceira forma de saída do JS é através do (alert) */
+alert(10+4);
+
+/*Essa é a quarta forma de saída do JS */
+console.log("Oi, isso é um console.log")
