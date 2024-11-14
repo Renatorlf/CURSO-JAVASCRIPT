6d3a7a0fa6a64c68a787a2a98327491bc1c8043a
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2024-11-13 23:03:43 -0300

    Declaraçoes

---
+++ JavaScript/declaracoes.js
@@ -0,0 +1,21 @@
+/*-declarações são instruções que podem declarar variáveis, funções, ou constantes.
+-No JavaScript, o escopo de uma variável ou função define a sua visibilidade dentro do programa. Uma variável ou função declarada num determinado escopo só pode ser acessada dentro desse escopo ou de escopos internos a ele. 
+-as Declarações são finalizadas com ponto e virgula(;)*/
+let a,b,c;
+a=5;
+b=6;
+c=7;
+
+/*O JavaScript ignora espaços em branco*/
+let pessoa = "Renato";
+let pessoa1="Renato";
+
+/*O JavaScript aceita quebras de linas na mesma declaração desde que finalize com ponto e vírgula para o mesmo entender o encerramento da declaração */
+
+document.getElementById("texto").innerHTML="Meu primeiro texto <b>JS</b>!";
+
+document.
+getElementById("texto").
+innerHTML="Meu primeiro texto <b>JS</b>!";
+
+alert('Óla, boa noite');
