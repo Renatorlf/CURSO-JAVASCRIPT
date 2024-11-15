a3b9a804cfb1a0b1b54d53e296ed2e1fd1daa313
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2024-11-15 10:39:07 -0300

    Aula 5 comentarios

--- JavaScript/comentarios.js
+++ JavaScript/comentarios.js
@@ -1,3 +1,6 @@
 // Para comentar apenas uma linha se Utiliza (//)
 
 /**Para comentários com mais de uma linha se utiliza, dessa forma que está com barra, asterisco, asterisco, barra*/
+
+/**O getElementByClassName, tem que colocar o índice da lista depois dos parenteses onde esta a classe, porque o JavaScript entende que várias classes seriam uma lista */
+document.getElementsByClassName("texto")[0].innerHTML="houver um erro ao criar os arquivos da aula, por isso que vai estar um pouco diferente no reporsitório do GitHub"
