b1a0aa9c92ec4da82deb6a2be49913119e7839d7
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2024-11-16 23:39:36 -0300

    Fucao

---
+++ JavaScript/funções.js
@@ -0,0 +1,40 @@
+/* FUNÇÕES
+uma função JavaScript é um bloco de código projetado para executar uma farefa especifica.
+
+É como uma pequena 'Fábrica' onde você faz uma entrada e ele te dá uma saída.
+
+Pode ser encarado como "mini-programas" projetados para fazer uma tarefa que vai contribuir para todo código 
+
+Uma função JavaScript é executada quando "algo" a invoca (chama)*/
+
+//FUNÇÃO DE SOMA
+/*function soma(valor1, valor2){
+    return valor1 + valor2;
+}
+
+document.getElementById("texto").innerHTML = soma(15, 30)*/
+
+//FUNÇÃO DE COTAÇÃO DO DOLAR
+/*
+function realParaDolar(real, cotacaoDolar){
+    return cotacaoDolar * real;
+
+}
+
+var valorReal = 7.89
+var cotacao = 5.8
+
+total = realParaDolar(valorReal, cotacao)
+alert("O valor em Real é R$: "+ valorReal + " e o valor em dolar é U$: "+ total)*/
+
+//Chamando a função através de um botão
+/*function alertaHello(){
+    alert("E ai galera, blz?")
+}
+*/
+
+function alertaHello(){
+    alert("E ai galera, blz?")
+}
+
+alertaHello();
