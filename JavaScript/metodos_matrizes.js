1a9d1d9d87ed6a5a7a9a38d9f9a27466ba0cb05f
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2024-11-24 22:31:38 -0300

    Metodos para matrizes

---
+++ JavaScript/metodos_matrizes.js
@@ -0,0 +1,45 @@
+//const pessoa = ["Paulo", "Silva", 20, "Professor"]
+//document.getElementById("teste").innerHTML = pessoa
+
+/*O método join() junta todos os elementos de um array (ou um array-like object) em uma string e retorna esta string.*/
+//document.getElementById("teste").innerHTML = pessoa.join(";")
+
+//--> Metodo (pop) remove o utlimo elemento do array
+/*pessoa.pop();
+document.getElementById("teste").innerHTML = pessoa*/
+
+//--> Metodo (push) adiciona mais um elemento no final do array
+/*pessoa.push("Moreno");
+document.getElementById("teste").innerHTML = pessoa*/
+
+//--> metodo (shift) remove o primeiro elemento do array
+/*pessoa.shift();
+document.getElementById("teste").innerHTML = pessoa*/
+
+//--> Metodo (unshift) adiciona um elementono começo do array
+/*pessoa.unshift("Ruivo");
+document.getElementById("teste").innerHTML = pessoa*/
+
+//--> Metodo (splice) adiciona multiplos elementos em um array na posição indicada.
+/*(pessoa.splice(2, 0, "adicionado 1", "Adicionado 2");
+document.getElementById("teste").innerHTML = pessoa*/
+
+//const lista1 = ["Arroz", "Feijão", "Macarrão", "Cuzcus"];
+//const lista2 = ["Manga", "Pêra", "Manga", "limão"];
+
+//--> Metodo (concat) contatena (junta) dois arrays.
+//const lista3 = lista1.concat(lista2);
+//document.getElementById("teste").innerHTML = lista3
+
+//--> Metodo (slice) fatia um array
+/*const frutas = ["Manga", "Pêra", "Manga", "limão", "Melância", "Goiaba", "Abacaxi"];*/
+/*const frutasFatiadas = frutas.slice(2);
+document.getElementById("teste").innerHTML = frutasFatiadas*/
+
+//--> Metodo (sort) deixa o array em ordem alfabética.
+//frutas.sort();
+//frutas.reverse();//Mostra na ordem inversa
+
+//const frutasFatiadas = frutas.slice(2);
+//document.getElementById("teste").innerHTML = frutas
+
